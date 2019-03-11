3ae7661efb5a3d5802589e45a3fb4f7cec4558b2
nanbokku <n1510126@edu.cc.uec.ac.jp>
2019-03-11 18:32:08 +0900

    [update] Make it possible to build jsx file using webpack.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, '/public/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
